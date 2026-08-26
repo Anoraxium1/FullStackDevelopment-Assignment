4b33ada3cd20a224aaf031c858189aa14087700f
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-08-26 23:00:18 +1000

    Added chat page

--- Fabulari/src/app/app.routes.ts
+++ Fabulari/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Chat } from './chat/chat';
 
 export const routes: Routes = [
     { path: '', component: Login, title: 'Login' },
+    { path: 'chat', component: Chat, title: 'Chat' },
 ];
