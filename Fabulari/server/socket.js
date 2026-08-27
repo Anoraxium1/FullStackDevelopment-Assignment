e29fc355b9ab62990e12a2046ec37ed97c716393
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-08-27 10:51:38 +1000

    added ability to change groups and rooms

---
+++ Fabulari/server/socket.js
@@ -0,0 +1,15 @@
+function initializeSockets(io) {
+  io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+
+    socket.on('chat message', (message) => {
+      io.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+    });
+  });
+}
+
+module.exports = initializeSockets;
--- Fabulari/src/app/chat/chat.ts
+++ Fabulari/src/app/chat/chat.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,4 +7,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
-export class Chat {}
+export class Chat {
+  protected readonly selectedGroup = signal(1);
+  protected readonly selectedRoom = signal(1);
+  protected readonly infoTab = signal<'info' | 'age' | 'colour'>('info');
+
+  selectGroup(id: number) {
+    this.selectedGroup.set(id);
+  }
+
+  selectRoom(id: number) {
+    this.selectedRoom.set(id);
+  }
+
+  setInfoTab(tab: 'info' | 'age' | 'colour') {
+    this.infoTab.set(tab);
+  }
+}
